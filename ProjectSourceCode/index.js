7ff3acbb6773953d2f5b1116107639f8d49b55fb
Sashi Wolf <sashiwolf@Sashis-MacBook-Pro.local>
2025-11-02 14:24:22 -0700

    Fixed database variable naming

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -9,16 +9,16 @@ const Handlebars = require('handlebars');
 const path = require('path');
 const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
 const bodyParser = require('body-parser');
-const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store
 const bcrypt = require('bcryptjs'); //  To hash passwords
-const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+const axios = require('axios'); // To make HTTP requests from our server
 app.use(express.static(path.join(__dirname, 'images')));
 
 // *****************************************************
 // <!-- Connect to DB -->
 // *****************************************************
 
-// create `ExpressHandlebars` instance and configure the layouts and partials dir.
+// create `ExpressHandlebars` instance and configure the layouts and partials dir
 const hbs = handlebars.create({
   extname: 'hbs',
   layoutsDir: __dirname + '/views/layouts',
@@ -50,7 +50,6 @@ db.connect()
   // <!-- App Settings -->
   // *****************************************************
   
-  // Register `hbs` as our view engine using its bound `engine()` function.
   app.engine('hbs', hbs.engine);
   app.set('view engine', 'hbs');
   app.set('views', path.join(__dirname, 'views'));
