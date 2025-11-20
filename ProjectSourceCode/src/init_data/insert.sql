5d42959626473ea823414dc1567bf9852d4868e5
ShonButz <gregory.butz05@gmail.com>
2025-11-20 11:11:01 -0700

    SQL stuff

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,5 +1,4 @@
 -- Enable bcrypt for seeding only (so we can insert plain passwords here)
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- =========================
 -- Users (password = Password1!)
@@ -10,7 +9,7 @@ INSERT INTO users (username, password, email, phone_num, role) VALUES
   ('user3', crypt('Password1!', gen_salt('bf')), 'ijkl9012@colorado.edu', '3333333300', 'user'),
   ('user4', crypt('Password1!', gen_salt('bf')), 'mnop3456@colorado.edu', '4444444400', 'user'),
   ('user5', crypt('Password1!', gen_salt('bf')), 'qrst7890@colorado.edu', '5555555500', 'user'),
-  ('user6', crypt('Password1!', gen_salt('bf')), 'uvwx2345@colorado.edu', '6666666600', 'user'),
+  ('user6', crypt('Password1!', gen_salt('bf')), 'uvwx2345@colorado.edu', '6666666600', 'user');
 
 
 
