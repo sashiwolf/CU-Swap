bad05f7fd006b615288c27d118d7e4a66b9e07b5
Sashi Wolf <sashiwolf@Sashis-MacBook-Pro.local>
2025-11-20 16:29:29 -0700

    Fixed nodemailer

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -99,9 +99,11 @@ db.connect()
     console.log('ERROR:', error.message || error);
   });
 
-
-  const transporter = nodemailer.createTransport({
-  service: 'gmail',
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,          // STARTTLS
+  secure: false,      // must be false for port 587
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
