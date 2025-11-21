01c95b9435bf6286f4dd4e7c206086aecad577c6
Sashi Wolf <sashiwolf@Sashis-MacBook-Pro.local>
2025-11-21 09:03:41 -0700

    Still trying things to see if nodemailer will work

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -105,8 +105,9 @@ db.connect()
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,         // Render can block 465; 587 uses STARTTLS
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
